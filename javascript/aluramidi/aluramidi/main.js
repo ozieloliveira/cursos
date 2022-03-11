ee016a40110b1117965c0f12b4ad373d2af035e5
ozieloliveira <ozioliveira8@gmail.com>
2022-03-11 12:09:11 -0300

    curso html css

---
+++ javascript/aluramidi/aluramidi/main.js
@@ -0,0 +1,10 @@
+
+function tocaSomPom() {
+         document.querySelector('#som_tecla_pom').play();
+}
+       const listaDeTeclas = document. querySelectorAll ('.tecla_pom');
+
+        listaDeTeclas[0].onclick = tocaSomPom; // essa linha eu subtraio a taxa do total
+
+
+  
