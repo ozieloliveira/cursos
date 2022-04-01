e1c44154531f481f2c5ae5ee0442dc16ddd30782
ozieloliveira <ozioliveira8@gmail.com>
2022-04-01 16:49:33 -0300

    revisão

---
+++ angular_formularios-main/angular_formularios-main/gatitobook/src/app/home/home.module.ts
@@ -0,0 +1,17 @@
+import { MensagemModule } from './../componentes/mensagem/mensagem.module';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { HomeRoutingModule } from './home-routing.module';
+import { HomeComponent } from './home.component';
+import { LoginComponent } from './login/login.component';
+import { FormsModule } from '@angular/forms';
+import { AutenticacaoService } from '../autenticacao/autenticacao.service';
+
+@NgModule({
+  declarations: [HomeComponent, LoginComponent],
+  imports: [CommonModule, HomeRoutingModule, FormsModule, MensagemModule],
+  providers: [AutenticacaoService],
+  exports: [HomeComponent],
+})
+export class HomeModule {}
